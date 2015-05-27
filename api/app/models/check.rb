0118fe0e91ce171ac696991377615b50bd9b8641
numidiasoft <tarik.ihadjadene@tigerlilyapps.com>
2015-05-27 16:31:42 +0200

    Add some feilds

--- api/app/models/check.rb
+++ api/app/models/check.rb
@@ -2,19 +2,24 @@ require "mongoid"
 module Sentinel
   class Check
     include Mongoid::Document
+    include Mongoid::Timestamps::Created
+    include Mongoid::Timestamps::Updated
+
     field :name, type: String
     field :url, type: String
     field :type, type: String
     field :protocol, type: String
+    field :updated_at, default: Time.now
     field :expected_response, type: String, default: :none
+    field :description, type: String
     field :status, type: String, default: 'green'
     validates :name, presence: true
     validates :url, presence: true
     validates :type, presence: true
     validates :protocol, presence: true
     validates :expected_response, presence: true
-    validates :status, inclusion: { in: ['warning', 'green', 'yellow'] }
-    validates :type, inclusion: { in: ['GET', 'POST', 'PATCH'] }
+    validates :status, inclusion: { in: ['red', 'green', 'yellow'] }
+    validates :type, inclusion: { in: ['auto', 'manual'] }
 
   end
 end
