40fa00fa28de36fe6274a2051935dcd51937d790
numidiasoft <tarik.ihadjadene@tigerlilyapps.com>
2015-05-27 16:39:22 +0200

    Add safly method to rescue all excpetion as first step

--- api/lib/healthcheck/http_status.rb
+++ api/lib/healthcheck/http_status.rb
@@ -16,17 +16,32 @@ module Sentinel
       end
 
       def check_with_get check_entry
-        response = Faraday.get(check_entry.url.to_s)
-        body_status = if check_entry.expected_response == "none"
-                        true
-                      else
-                        check_entry.expected_response == response.body
-                      end
-        Level::Http.service_state(response.status, body_status)
+        safly do
+          response = Faraday.get(check_entry.url.to_s)
+          body_status = if check_entry.expected_response == "none"
+                          true
+                        else
+                          check_entry.expected_response == response.body
+                        end
+          Level::Http.service_state(response.status, body_status)
+        end
       end
 
       def check_with_post check
         #TODO
+      end
+
+      private
+      def logger
+        Sentinel.logger
+      end
+
+      def safly
+        begin
+          yield
+        rescue Exception => e
+          logger.info(e.message)
+        end
       end
     end
   end
