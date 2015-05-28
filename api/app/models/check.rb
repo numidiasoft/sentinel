824098d84e7818bf6c3726ab342c00e10a441ca9
numidiasoft <tarik.ihadjadene@tigerlilyapps.com>
2015-05-28 12:11:57 +0200

    Add verb and params field

--- api/app/models/check.rb
+++ api/app/models/check.rb
@@ -11,6 +11,8 @@ module Sentinel
     field :protocol, type: String
     field :expected_response, type: String, default: :none
     field :description, type: String
+    field :verb, type: String
+    field :params, type: String
     field :status, type: String, default: 'green'
     validates :name, presence: true
     validates :url, presence: true
@@ -19,6 +21,7 @@ module Sentinel
     validates :expected_response, presence: true
     validates :status, inclusion: { in: ['red', 'green', 'yellow'] }
     validates :type, inclusion: { in: ['auto', 'manual'] }
+    validates :verb, inclusion: { in: ['GET', 'POST', 'PATCH'] }
 
   end
 end
