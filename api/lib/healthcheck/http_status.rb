3380e6f5090b4a126e40cad8f93cbbddb3d43fcb
numidiasoft <tarik.ihadjadene@tigerlilyapps.com>
2015-05-28 12:14:17 +0200

    Add check for post http request

--- api/lib/healthcheck/http_status.rb
+++ api/lib/healthcheck/http_status.rb
@@ -5,12 +5,13 @@ module Sentinel
 
     class << self
       def check(check_entry)
-        case check_entry.type
+        case check_entry.verb
         when "GET"
           check_entry.update_attribute(:status, check_with_get(check_entry))
         when "POST"
+          check_entry.update_attribute(:status, check_with_post(check_entry))
         else
-          logger.error("Unknown method for #{self.class}")
+          logger.error("Unknown method #{check_entry.verb} #{self.inspect}")
         end
         check_entry
       end
@@ -18,22 +19,27 @@ module Sentinel
       def check_with_get check_entry
         safly do
           response = Faraday.get(check_entry.url.to_s)
-          body_status = if check_entry.expected_response == "none"
-                          true
-                        else
-                          check_entry.expected_response == response.body
-                        end
-          Level::Http.service_state(response.status, body_status)
+          check_body(response, check_entry)
         end
       end
 
-      def check_with_post check
-        #TODO
+      def check_with_post check_entry
+        response = Faraday.post(check_entry.url, try_json(check_entry.params))
+        check_body(response, check_entry)
       end
 
       private
       def logger
         Sentinel.logger
+      end
+
+      def check_body(response, check_entry)
+        body_status = if check_entry.expected_response == "none"
+                        true
+                      else
+                        check_entry.expected_response == response.body
+                      end
+        Level::Http.service_state(response.status, body_status)
       end
 
       def safly
@@ -42,6 +48,10 @@ module Sentinel
         rescue Exception => e
           logger.info(e.message)
         end
+      end
+
+      def try_json(params)
+        JSON.parse(params) rescue params
       end
     end
   end
