e592b54a2a7251a575cb1bbf49f0c62e8eff4f5e
numidiasoft <tarik.ihadjadene@tigerlilyapps.com>
2015-07-09 17:10:58 +0200

    Add whenever integration

---
+++ api/config/schedule.rb
@@ -0,0 +1,13 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+set :output, './cron.log'
+
+require_relative 'application'
+
+every 1.minutes do
+  command "RACK_ENV=#{ENV['RACK_ENV']} cd /app && bundle exec ruby ./script/check_statues.rb"
+end
+
